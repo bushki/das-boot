beb0729e072317c37dd5247b66c674e3d04a83bd
Luis <luis.p.melgar@disney.com>
2019-06-16 14:37:29 -0700

    added method to get wreck by ID

--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -2,6 +2,7 @@ package com.boot.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,5 +22,10 @@ public class ShipwreckController {
 	@RequestMapping(value ="shipwrecks", method=RequestMethod.POST)
 	public Shipwreck create(@RequestBody Shipwreck shipwreck) {
 		return ShipwreckStub.create(shipwreck);
+	}
+	
+	@RequestMapping(value ="shipwrecks/{id}", method=RequestMethod.GET)
+	public Shipwreck get(@PathVariable long id) {
+		return ShipwreckStub.get(id);
 	}
 }
